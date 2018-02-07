75f26a5800cbc4a261d3ec47ffe79e8acfea4496
Alan Platt <aplatt@equalexperts.com>
2018-02-07 17:37:40 +0000

    simply entrypoint.sh
    use specific urls for dsl files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e
+usage() {
+  echo """
+  USAGE
+  --jobdslurls='url1;url2'  List urls of jobdsl files
+  """
+  exit 0
+}
 
 main() {
 
@@ -8,8 +14,11 @@ main() {
     PARAM=$(echo "$1" | awk -F= '{print $1}')
     VALUE=$(echo "$1" | awk -F= '{print $2}')
     case $PARAM in
-        --gitrepos)
-            OPTION_GITREPOS="${VALUE}"
+        --help)
+            usage
+            ;;
+        --jobdslurls)
+            OPTION_JOB_DSL_URLS="${VALUE}"
             ;;
         *)
             JENKINS_PARAMS="${JENKINS_PARAMS} ${1}"
@@ -18,10 +27,14 @@ main() {
     shift
   done
 
-  for REPO in $(echo "${OPTION_GITREPOS}" | tr ";" "\n"); do
-    REPO_NAME=${REPO##*/}
-    curl -s "${REPO}/master/${REPO_NAME}.jobdsl" -o "/usr/share/jenkins/ref/jobdsl/${REPO_NAME}.jobdsl"
+  for DSL_FILE_URL in $(echo "${OPTION_JOB_DSL_URLS}" | tr ";" "\n"); do
+    JOBDSL_DIR="/var/jenkins_home/jobdsl"
+    mkdir -vp "${JOBDSL_DIR}"
+    echo "From ${DSL_FILE_URL} to ${JOBDSL_DIR}"
+    wget -q "${DSL_FILE_URL}" -P "${JOBDSL_DIR}"
   done
+
+  echo "START:"
 
   /bin/bash -c "/usr/local/bin/jenkins.sh ${JENKINS_PARAMS}"
 
