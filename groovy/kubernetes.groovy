8dfb9b5ade6780b2314be058785aeda045933d5a
Alan Platt <aplatt@equalexperts.com>
2018-03-12 16:07:31 +0000

    add buildpod for building containers
    This includes a docker-uml container or building containers in an
    unprivileged container and a kubectl container for deploying to the
    cluster

--- groovy/kubernetes.groovy
+++ groovy/kubernetes.groovy
@@ -1,6 +1,8 @@
 println 'Configuring Kubernetes plugin'
 
 import org.csanchez.jenkins.plugins.kubernetes.*
+import org.csanchez.jenkins.plugins.kubernetes.model.*;
+import org.csanchez.jenkins.plugins.kubernetes.volumes.*;
 import jenkins.model.*
 
 def jenkins = Jenkins.getInstance()
@@ -33,5 +35,39 @@ JenkinsSlavePod.setContainers([JnlpContainer])
 
 kubernetes.addTemplate(JenkinsSlavePod)
 
+def KubectlContainer = new ContainerTemplate('kubectl')
+KubectlContainer.setName('kubectl')
+KubectlContainer.setImage('lachlanevenson/k8s-kubectl:latest')
+KubectlContainer.setCommand('cat')
+KubectlContainer.setArgs('')
+KubectlContainer.setTtyEnabled(true)
+
+def DockerUML = new ContainerTemplate('docker')
+DockerUML.setName('docker')
+DockerUML.setImage('microdc/docker-uml:latest')
+DockerUML.setArgs('sleep inf')
+DockerUML.setTtyEnabled(true)
+DockerUML.setEnvVars([
+  new KeyValueEnvVar('DISK', '90G'),
+  new KeyValueEnvVar('MEM', '4G'),
+  new KeyValueEnvVar('AWS_DEFAULT_REGION', 'eu-west-1'),
+  new SecretEnvVar('AWS_ACCESS_KEY_ID', 'aws', 'AWS_ACCESS_KEY_ID'),
+  new SecretEnvVar('AWS_SECRET_ACCESS_KEY', 'aws', 'AWS_SECRET_ACCESS_KEY'),
+])
+
+def buildpod = new PodTemplate()
+buildpod.setName('buildpod')
+buildpod.setNamespace('jenkins')
+buildpod.setLabel('buildpod')
+buildpod.setVolumes([
+  new SecretVolume('/home/jenkins/.ssh', 'jenkins-ssh-keys'),
+])
+buildpod.setContainers([KubectlContainer, DockerUML])
+
+kubernetes.addTemplate(buildpod)
+kubernetes.addTemplate(JenkinsSlavePod)
+
 jenkins.clouds.replace(kubernetes)
 jenkins.save()
+
+println 'Kubernetes plugin configuration complete'
